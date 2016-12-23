2753205aaba7783e76e4c0c6df89dac32167a2ce
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-12-23 19:21:45 +0100

    test codeceptjs

---
+++ tests/login_test.js
@@ -0,0 +1,14 @@
+/// <reference path="./steps.d.ts" />
+Feature('Login');
+
+Scenario('test successful login', (I) => {
+    // I.fillField('q','ole.org');
+    // I.click('btnG');
+    // I.seeInTitle('ole.org');
+    I.amOnPage('/');
+    I.fillField('Login', 'admin');
+    I.fillField('Password', 'password');
+    I.click('Sign In');
+    I.waitForText('Manager', 10);
+    I.seeInCurrentUrl('#dashboard');
+});
