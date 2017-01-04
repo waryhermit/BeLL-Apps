f1d19f959f88ef88cc755531d2d81aa8f522bce8
dogi <dogi@users.noreply.github.com>
2017-01-04 17:55:07 -0500

    added codecept.conf.js and more (#50)

--- tests/login_test.js
+++ tests/login_test.js
@@ -9,6 +9,8 @@ Scenario('test successful login', (I) => {
     I.fillField('Login', 'admin');
     I.fillField('Password', 'password');
     I.click('Sign In');
-    I.waitForText('Manager', 10);
-    I.seeInCurrentUrl('#dashboard');
+    // I.waitForText('Manager', 10);
+    // I.seeInCurrentUrl('#dashboard');
+    I.waitForText('Set Configurations', 10);
+    I.seeInCurrentUrl('#configuration/add');
 });
