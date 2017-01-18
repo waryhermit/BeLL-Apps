98e1cf72248ecc46ae2a360cc942762a0f218d69
waryhermit <waryhermit@live.com>
2017-01-18 18:00:23 -0500

    inital automatic inno build

---
+++ installer/innoinstall.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+rm -rf /tmp/inno
+mkdir /tmp/inno
+cd /tmp/inno
+wget -O is.exe http://files.jrsoftware.org/is/5/isetup-5.5.5.exe
+innoextract is.exe
+mkdir -p ~/".wine/drive_c/inno"
+cp -a app/* ~/".wine/drive_c/inno"
