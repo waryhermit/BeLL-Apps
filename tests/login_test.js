cdeb8c4a314e2c140c5b4654b9e23eaf7528fca4
waryhermit <waryhermit@live.com>
2017-01-27 16:20:14 -0500

    inital resuable login commit

--- tests/login_test.js
+++ tests/login_test.js
@@ -1,14 +1,16 @@
 /// <reference path="./steps.d.ts" />
 Feature('Login');
+var FirstLogin = false;
+var loginCookies = [];
+
+Before((I) => {
+    I.login('admin', 'password');
+
+});
 
 Scenario('test successful login', (I) => {
-    // I.fillField('q','ole.org');
-    // I.click('btnG');
-    // I.seeInTitle('ole.org');
-    I.amOnPage('/');
-    I.fillField('Login', 'admin');
-    I.fillField('Password', 'password');
-    I.click('Sign In');
-    I.waitForText('Manager', 10);
     I.seeInCurrentUrl('#dashboard');
+    I.wait(60);
+    I.click('Logout');
+    I.login('admin', 'password');
 });
--- tests/steps_file.js
+++ tests/steps_file.js
@@ -1,12 +1,29 @@
 
-'use strict';
+//'use strict';
 // in this file you can append custom step methods to 'I' object
+module.exports = function () {
 
-module.exports = function() {
+  var loginCount = 0; // Used to check for config page.
+  var loginCookies = [];
   return actor({
 
     // Define custom steps here, use 'this' to access default methods of I.
-    // It is recommended to place a general 'login' function here.
-    
+    // It is recommended to place a general 'login' fSunction here.
+
+    login: function (email, password) {
+      if (loginCount < 1) {
+        this.amOnPage('/');
+        this.fillField('Login', 'admin');
+        this.fillField('Password', 'password');
+        this.click('Sign In');
+        this.saveCookies();
+      }
+      else {
+        this.loadCookies();
+        this.amOnPage('/');
+        this.seeInCurrentUrl('#dashboard');
+      }
+      loginCount = loginCount + 1;
+    }
   });
 }
