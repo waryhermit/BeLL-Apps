731283b301f1c7e62a8f1c38f4aa32f954dffb49
waryhermit <waryhermit@live.com>
2017-02-01 14:53:09 -0500

    check and accept popup

--- tests/steps_file.js
+++ tests/steps_file.js
@@ -24,6 +24,8 @@ module.exports = function () {
           this.fillField(fields[fn], 'ole');
         }
         this.click("Submit Configurations");
+        this.seeInPopup("Configurations are successfully added.")
+        this.acceptPopup();
         this.seeInCurrentUrl('#dashboard');
       }
       else {
