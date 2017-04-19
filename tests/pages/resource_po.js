c40522732880db5516389e6d80eb7b1b454f9aad
Evan P <waryhermit@live.com>
2017-04-19 11:28:38 -0400

    fix stupid typo

--- tests/pages/resource_po.js
+++ tests/pages/resource_po.js
@@ -145,7 +145,7 @@ module.exports = {
     fill_resource(resource_form) {
         // TODO: if they don't specify Subject, Level, or collections select all.
         // TODO: if there is a file incuded upload it.
-        I.waitForVisible((this.feilds.title);
+        I.waitForVisible(this.feilds.title);
         I.wait(1);
         I.fillField(this.feilds.title, resource_form.title);
         I.fillField(this.feilds.author, resource_form.author);
